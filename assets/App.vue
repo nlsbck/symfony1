2bf5fef8808c029dbeb84f910611a5963e058372
nlsbck <nils-bock@t-online.de>
2022-10-26 14:17:50 +0200

    playing around

---
+++ assets/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <h2>Welcome</h2>
+    <button @click="counter++">Click me {{counter}}</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "App",
+  data: () => ({
+    counter: 0
+  })
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/Entity/Game.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity]
+class Game
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private int $id;
+
+    #[ORM\Column(length: 255)]
+    private string $title;
+}
